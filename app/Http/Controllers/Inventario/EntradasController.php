09b1281748ea2664b93cfaa091e1327261e9d0b0
ymendoza <yoe318@gmail.com>
2018-09-12 16:52:00 -0400

    ajuste de galeria, entrada inventario

--- app/Http/Controllers/Inventario/EntradasController.php
+++ app/Http/Controllers/Inventario/EntradasController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Inventario;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Proveedores;
 class EntradasController extends Controller
 {
     public function index(){
-    	return view('Inventario.Entradas.index');
+    	$proveedores = proveedores::where('id_estado','1')->get();
+    	return view('Inventario.Entradas.index')->with('proveedores',$proveedores);
     }
     public function show(){
-    	return view('Inventario.Entradas.show');
+    	$proveedores = proveedores::where('id_estado','1')->get();
+    	return view('Inventario.Entradas.show')->with('proveedores',$proveedores);
     }
 }
