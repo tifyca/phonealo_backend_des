53351a39316e14ac6bf178d36c806640205399a7
Jonathan Gonzalez <thannajo@gmail.com>
2018-09-19 20:18:46 -0400

    Modulo Logistica listado de Ventas

--- app/Http/Controllers/Procesar/LogisticaController.php
+++ app/Http/Controllers/Procesar/LogisticaController.php
@@ -5,10 +5,30 @@ namespace App\Http\Controllers\Procesar;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use DB;
+use App\Ventas;
+use App\Ciudades;
+use App\Horarios;
+
 class LogisticaController extends Controller
 {
     public function index(){
-    	return view('Procesar.Logistica.index');
+    	
+    	$ventas = DB::table('ventas')
+    		->join('pedidos', 'ventas.id_pedido', '=', 'pedidos.id')
+    		->join('clientes', 'pedidos.id_cliente', '=', 'clientes.id')
+    		->join('ciudades', 'clientes.id_ciudad', '=', 'ciudades.id')
+    		->select('ventas.*', 'pedidos.id_cliente', 'clientes.nombres', 'clientes.telefono', 'clientes.direccion', 'ciudades.ciudad')
+    		->where('ventas.id_estado', '=', '1')
+    		->where('ventas.status_v', '=', '11')
+    		->orwhere('ventas.id_estado', '=', '11')
+    		->where('ventas.status_v', '=', '11')
+    		->orderby('ventas.horario_entrega', 'desc')
+    		->get();
+    	$ciudades = Ciudades::get();
+    	$horarios = Horarios::get();
+    #dd($horarios);
+    	return view('Procesar.Logistica.index', compact('ventas', 'ciudades', 'horarios'));
     }
     public function edit(){
     	return view('Procesar.Logistica.edit');
