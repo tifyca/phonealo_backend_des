04bdc98194b66b88e4693aab6b0f7270f531d309
irene16 <irenemontilla16@gmail.com>
2018-10-03 00:31:40 -0400

    prueba factura

--- resources/views/Procesar/Logistica/factura.blade.php
+++ resources/views/Procesar/Logistica/factura.blade.php
@@ -117,13 +117,30 @@
       </style>
 
 </head>
-<body>
 
+<body align="center">
+        <script type="text/javascript">
+    
+function sep(cant){
+  var c = cant;
+  if(c.length<7){
+    var b = c.substring(0, c.length-3)+'.'+c.substring(c.length-3, c.length);
+  }else if(c.length>6 && c.length<10){
+    var b = c.substring(0, c.length-6)+'.'+c.substring(c.length-6, c.length-3)+'.'+c.substring(c.length-3, c.length);
+  }else{
+    var b = c.substring(0, c.length-9)+'.'+c.substring(c.length-9, c.length-6)+'.'+c.substring(c.length-6, c.length-3)+'.'+c.substring(c.length-3, c.length);
+  }
+  
+  return b;
+}
+ </script>
 
       @foreach($venta as $item)  
       <br><br><br><br><br>
   <table class=" table table-hover" width="100%">
-
+<tr>
+  <td></td>
+</tr>
 <tr>
  <td  style="padding-left:5px; padding-top: 2px; font-size: 12px;" id="nom">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$item->nombres}}</td>
 <td  style="padding-left:5px; padding-top: 2px; font-size: 12px;"  >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X
@@ -139,7 +156,56 @@
 </tr>
   </table>
   <br>
-  @endforeach
+  <table class=" table table-hover"   width="100%">
+           
+        
+                <?php $l=0; $timporte=0; $subimporte=0; ?>
+                @foreach($factura as $item2)  
+                <?php  $l= $l+1; ?>
+              
+                <tr>
+                  <td id="cantidad" align="center" style="font-size: 12px; width: 1px;">{{$item2->cantidad}}</td>
+                  <?php if($item2->nombre_original==""||$item2->nombre_original==NULL){?>     
+                  <td colspan="4" id="producto" align="left" style="font-size: 12px width: 79px;">{{$item2->descricion}}</td>
+                  <?php }else{?>
+                  <td colspan="4" id="producto" align="left" style="font-size: 12px; width: 79px;">{{$item2->nombre_original}}</td>
+                  <?php } ?>
+                  <td style="width: 10px; font-size: 12px;">{{$item2->precio}}</td>
+                  <?php $subimporte= $item2->cantidad * $item2->precio;
+                  $timporte+=$subimporte;?>
+                  <td id="importe" style="font-size: 12px; width: 10px;" align="center">{{$subimporte}}</td>
+                @endforeach 
+                </tr>
+                @for ($i = $l; $i < 9; $i++)
+                <tr>
+                  <td id="cantidad" align="center" style="color: gray; width: 1px;">_</td>
+                  <td   colspan="4" id="producto" align="left" style="color: gray; width: 79px;">_</td>
+                    <td style="width: 10px; color: gray;">_</td>
+                    <td style="width: 10px; color: gray;" align="center">_</td>
+                  
+                  </tr>
+               @endfor
+                  <tr>
+                    <td align="left" style="width: 1px;" ></td>
+                    <td colspan="4" style="width:79px;"></td>
+                    <td style="width: 10px;"></td>
+                    <td align="center" style="font-size: 12px; width: 10px; ">{{$item->precio}}</td>
+                  </tr>
+              @endforeach 
+                  <tr>
+                    <td align="center"  colspan="5" style="font-size: 12px;"> {!!NumerosEnLetras::convertir($timporte)!!}</td>
+                    <td style="width: 10px;"></td>
+                    <td align="center"  style="font-size: 12px;">{{$timporte}} <br><br>{!!number_format($timporte, 0, ',', '.')!!}</td>
+                  </tr>
+                  <tr>
+                    <td  align="left" >&nbsp;.</td>
+                    <td align="center" colspan="4" style="width: 10px;font-size: 12px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{!!number_format($timporte/11, 0, ',', '.')!!}</td>
+                    <td align="right" style="font-size: 12px;">{!!number_format($timporte/11, 0, ',', '.')!!}</td>
+                    <td  align="center" style="font-size: 12px;">&nbsp; </td>
+                  </tr>
+        
+              </table>
+
 
 {{--/////////////////////////////////////////////////////////////////////////////////////--}}
 
