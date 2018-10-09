4cb5d5b03409d3745dff97419752b6b06be8b149
Luis <leip1493@gmail.com>
2018-10-08 23:12:06 -0430

    Mostrando faltantes

--- app/Http/Controllers/Procesar/FaltantesController.php
+++ app/Http/Controllers/Procesar/FaltantesController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers\Procesar;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\pedido;
+use DB;
 
 class FaltantesController extends Controller
 {
     public function index(Request $request){
-        $pedidos = pedido::orderBy('id', 'ASC')->where('id_estado', 5)->paginate(10);
+
+        // $pedidos = DB::table('pedidos')
+            // ->leftjoin('clientes', 'pedidos.id_cliente', 'clientes.id')
+            // ->leftjoin('detalle_pedidos', 'pedidos.id', 'detalle_pedidos.id_pedido')
+            // ->leftjoin('productos', 'detalle_pedidos.id_producto', 'productos.id')
+            // ->leftjoin('categorias', 'productos.id_categoria', 'categorias.id')
+            // ->leftjoin('empleados', 'pedidos.id_usuario', 'empleados.id')
+            // ->leftjoin('ventas', 'pedidos.id', 'ventas.id_pedido')
+            // ->select('clientes.nombres', 'clientes.telefono', 'productos.codigo_producto', 'productos.descripcion', 'productos.stock_activo', 'categorias.categoria', 'detalle_pedidos.cantidad', 'empleados.nombres as nombresEmpleado', 'pedidos.id_usuario', 'ventas.fecha')
+            // ->where('pedidos.id_estado', 5)
+            // ->paginate(10);
+
+        $pedidos = pedido::enEspera()->paginate(10);
+        // return dd($pedidos);
+
        return view("Procesar.Faltantes.index", compact('pedidos')); 
     }
     public function show($id){
--- app/pedido.php
+++ app/pedido.php
@@ -10,6 +10,22 @@ class pedido extends Model
     protected $table = 'pedidos';
     protected $fillable = ['id','id_cliente','fecha','autor'];
 	
+	public function scopeEnEspera($query){
+		return $query->join('clientes', 'pedidos.id_cliente', 'clientes.id')
+            ->join('detalle_pedidos', 'pedidos.id', 'detalle_pedidos.id_pedido')
+            ->join('productos', 'detalle_pedidos.id_producto', 'productos.id')
+            ->join('categorias', 'productos.id_categoria', 'categorias.id')
+            ->join('empleados', 'pedidos.id_usuario', 'empleados.id')
+            ->join('ventas', 'pedidos.id', 'ventas.id_pedido')
+            ->select('clientes.nombres', 'clientes.telefono', 'productos.codigo_producto', 'productos.descripcion', 'productos.stock_activo', 'categorias.categoria', 'detalle_pedidos.cantidad', 'empleados.nombres as nombresEmpleado', 'pedidos.id_usuario', 'ventas.fecha')
+            ->where('pedidos.id_estado', 5)
+            ->orderBy('ventas.fecha', 'DESC');
+	}
+
+    ////////////////
+    // Relaciones //
+    ////////////////
+
 	public function venta(){
 		return $this->hasOne(Ventas::class, 'id_pedido');
 	}
@@ -17,6 +33,5 @@ class pedido extends Model
 	public function cliente(){
 		return $this->belongsTo(Clientes::class, 'id_cliente');
 	}
-
 
 }
