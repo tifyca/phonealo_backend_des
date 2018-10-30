91fb8390ccfe9685df78d6a325a6a6ac12039b45
ymendoza <yoe318@gmail.com>
2018-10-30 19:28:10 -0400

    ajuste de monitoreo

--- resources/views/Logistica/monitoreo.blade.php
+++ resources/views/Logistica/monitoreo.blade.php
@@ -14,85 +14,105 @@
 
 <?php $pos=1;   //dd($remitos);?>
 <div class="row">
+	<?php $valor=1; ?>
 	@foreach($repartidores as $repartidor)
-	<div class="col-lg-6 col-md-12">
-		<div class="card">
-			<div class="card-header card-header-warning">
-				<h4 class="card-title">Repartidor: {{$repartidor->nombres}}</h4>
-				<p class="card-category">Horario de Entrada</p>
-				<p class="card-category">Promedio</p><a class="card-link" href="#">Informe</a><a class="card-link" href="#">Ver Recorrido</a>
-			</div>
-			@php
-			$data=[];
-			foreach($gremitos as $gremito)
-			{
-				if($gremito->id_delivery==$repartidor->id_delivery)
-				{
-					$data[]=[
-						'id_remito' => $gremito->id_remito,
-						'importe' => $gremito->importe
+	<?php
+	$name="opcion".$valor;
+	$valor++;
+	
+	$data=[];
+	foreach($gremitos as $gremito)
+	{
+		if($gremito->id_delivery==$repartidor->id_delivery)
+		{
+			$data[]=[
+				'id_remito' => $gremito->id_remito,
+				'importe' => $gremito->importe
 
-					];
-				}	
-			} 
-			$ventas=[];
-			foreach($remitos as $remito)
-			{
-				if($remito->id_delivery==$repartidor->id_delivery)
-				{
-					$ventas[]=[
-						'id_venta' => $remito->id_venta,
-						'id_remito' => $remito->id_remito,
-						'id_estado' => $remito->id_estado
+			];
+		}	
+	} 
+	$ventas=[];
+	foreach($remitos as $remito)
+	{
+		if($remito->id_delivery==$repartidor->id_delivery)
+		{
+			$ventas[]=[
+				'id_venta' => $remito->id_venta,
+				'id_remito' => $remito->id_remito,
+				'id_estado' => $remito->id_estado
 
-					];
-				}	
-			} 	
+			];
+		}	
+	} 	
 					//dd($remitos);
-			@endphp
-			<div class="card-body table-responsive">
-				<table class="table table-hover">
+	?>
+	<div class="col-lg-6 col-md-12">
+		<div id="accordion">
+			<?php $label1="headingOne".$valor;
+			$label2="collapseOne".$valor;
+			?>
+			<div class="card">
+				<div class="card-header" id="{{$label1}}">
+					<h5 class="mb-0">
+						<button class="btn btn-link" data-toggle="collapse" data-target="#{{$label2}}" aria-expanded="true" aria-controls="collapseOne">
+							<h4 class="card-title">
+								Repartidor: {{$repartidor->nombres}}</h4>
 
-					@foreach($data as $data)
-					<thead class="text-warning">
-						<tr>
-							<th>Remito</th>	
-							<th colspan="4">{{ $data["id_remito"] }}</th>
-						</tr>
-						<tr>
-							<th>Pedido</th>
-							<th>Situacion</th>
-							<th>Horario </th>
-							<th>Importe</th>
-							<th>Chat</th>
-						</tr>
-					</thead>
-					<tbody>
-					  @foreach($ventas as $venta)
-						@if($data["id_remito"]==$venta["id_remito"])
-						<tr>
-							<td>{{ $venta["id_venta"] }}</td>
-							<td></td>
-							<td></td>
-							<td></td>
-							<td></td>
-						</tr>
-					    @endif	
-					</tbody>
-					@endforeach	
-				@endforeach	
-				</table>
+							</button><br>
+							<small>Horario de Entrada: 00:00:00</small><br>
+							<small>Promedio: 00</small><br>
+							<small><a class="card-link" href="#">Informe</a><br><a class="card-link" href="#">Ver Recorrido</a></small>
+							</h5>
+						</div>
+
+						<div id="{{$label2}}" class="collapse show" aria-labelledby="{{$label1}}" data-parent="#accordion">
+							<div class="card-body">
+								<div class="card-body table-responsive">
+									<table class="table table-hover">
+
+										@foreach($data as $data)
+										<thead class="text-warning">
+											<tr>
+												<th>Remito</th>	
+												<th colspan="4">{{ $data["id_remito"] }}</th>
+											</tr>
+											<tr>
+												<th>Pedido</th>
+												<th>Situacion</th>
+												<th>Horario </th>
+												<th>Importe</th>
+												<th>Chat</th>
+											</tr>
+										</thead>
+										<tbody>
+											@foreach($ventas as $venta)
+											@if($data["id_remito"]==$venta["id_remito"])
+											<tr>
+												<td>{{ $venta["id_venta"] }}</td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											@endif	
+										</tbody>
+										@endforeach	
+										@endforeach	
+									</table>
+								</div>				</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php $valor++;?>
+				@endforeach
+
+
 			</div>
-		</div>
-	</div>
-	@endforeach
-
-
-</div>
 
 
 
 
 
-
-@endsection
+			@endsection
