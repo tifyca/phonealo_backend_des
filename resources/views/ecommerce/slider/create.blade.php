3b0c767b8682154d16ad6a39fbfd8c9d45cdf83d
leiiraawr <leiditha@gmail.com>
2018-11-11 21:21:07 -0430

    controlador con edit y crear slider

--- resources/views/ecommerce/slider/create.blade.php
+++ resources/views/ecommerce/slider/create.blade.php
@@ -16,7 +16,7 @@
   <div class="col-12">
     <div class="tile">
       <div class="tile-body ">
-       <form name="form1" action="#" accept-charset="UTF-8"  method="post">
+       <form name="form1" action="{{route('slider.store')}}" accept-charset="UTF-8" enctype="multipart/form-data" method="post">
         {{ csrf_field() }}
 
         <div class="col-12 ">
@@ -33,7 +33,7 @@
              {{--  --}}
             <div class="col-lg-8">
                 <label for="titulo" ><b>Título</b></label>
-                <input class="form-control" placeholder="Título" name="titulo" id="titulo"  required>
+                <input class="form-control" placeholder="Título" name="descripcion" id="descripcion"  required>
                 
              </div>
             
@@ -48,7 +48,7 @@
               
                 <div class="form-group">
                   <label class="control-label">Seleccionar Imagen</label>
-                  <input class="form-control" type="file">
+                  <input type="file" class=" read-file read" id="archivo" name="archivo" accept="image/*">
                 </div>
               
             </div>
@@ -64,3 +64,64 @@
   </form>
 </div>
 @endsection
+
+@push('scripts')
+  <script type="text/javascript" language="javascript">
+    $ = jQuery;
+    $(function() {
+    $('#archivo').change(function(e) {
+      addImage(e); 
+    });
+
+    function addImage(e){
+      var file = e.target.files[0],
+      imageType = /image.*/;
+      if (!file.type.match(imageType))
+       return;
+
+     var reader = new FileReader();
+     reader.onload = fileOnload;
+     reader.readAsDataURL(file);
+   }
+
+   function fileOnload(e) {
+    var result=e.target.result;
+    $('#imgSalida').attr("src",result);
+    }
+  }); 
+
+  </script>
+   <script>
+    var editor_config = {
+      path_absolute : "{{ URL::to('/') }}/",
+      selector: "textarea",
+      plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+      ],
+      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+      relative_urls: false,
+      file_browser_callback : function(field_name, url, type, win) {
+        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+        if (type == 'image') {
+          cmsURL = cmsURL + "&type=Images";
+        } else {
+          cmsURL = cmsURL + "&type=Files";
+        }
+        tinyMCE.activeEditor.windowManager.open({
+          file : cmsURL,
+          title : 'Filemanager',
+          width : x * 0.8,
+          height : y * 0.8,
+          resizable : "yes",
+          close_previous : "no"
+        });
+      }
+    };
+    tinymce.init(editor_config);
+  </script>
+@endpush
